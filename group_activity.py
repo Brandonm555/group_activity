cb71ccdbdbbf05ed1841b73851eef1b4cd293862
Brandonm555 <brandonmdesign@gmail.com>
2016-08-10 19:09:13 -0700

    Add files via upload
    Finished checking performance of functions, left KennyLoggins as the player.

--- group_activity.py
+++ group_activity.py
@@ -1,31 +1,36 @@
 
 class Deck(object):
     def __init__(self):
-        cards = []
-        suits = ['H', 'C', 'S', 'D']
-        values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+        self.cards = []
 
-    def generateCards(self):
+    def generateCards(self, suits = ['H', 'C', 'S', 'D'], values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']):
         for suit in suits:
             for value in values:
                 self.cards.append(Card(suit, value))
 
     def shuffle(self):
+        # didn't quite get this to work out, but we decided to keep moving as none of us were near the minimum target for the day.
+		# i = len(self.card)
+		# while i > 0:
+		# 	rand_card = random.choice(self.card)
+		# 	temp = rand_card
+		# 	rand_card = self.card[i]
+		# 	self.card[i] = temp
+		# 	i -= 1
         pass
 
-class Cards(object):
-    def __init__(self, suit, value):
-        self.suit
-        self.value
+    def displayInfo(self):
+        print self.cards
 
-    def shuffle(self):
-		i = len(self.card)
-		while i > 0:
-			rand_card = random.choice(self.card)
-			temp = rand_card
-			rand_card = self.card[i]
-			self.card[i] = temp
-			i -= 1
+
+class Card(object):
+    def __init__(self, suit, value):
+        print "A new card has been created!"
+        self.suit = suit
+        self.value = value
+
+    def displayInfo(self):
+        print self.value, self.suit
 
 class Player(object):
     def __init__(self, name, hand):
@@ -33,54 +38,23 @@ class Player(object):
         self.name = name
         self.hand = []
 
-    def draw(self):
+    def draw(self,deck):
         # take an object "card" as "card(suit,value)" from object deck and add it to players hand  --- check
         # this will increase player's hand count by 1 and decrease deck count by 1
-        Deck.cards = ['H2','H3','H4','H5']
-        player.hand = []
-        player.hand.append(cards(len(cards -1)))
-        player.hand_count += 1
-        cards(len(cards -1)).pop # is pop redundant? I just want to make sure that that card with its suit and value are no longer in the deck
-        deck_count -= 1
+        self.hand.append(deck.cards[len(deck.cards) - 1])
+        deck.cards.pop() # is pop redundant? I just want to make sure that that card with its suit and value are no longer in the deck
 
-    def deal(self):
-        for x in len(player.hand):
-            if x <= 5:
-                draw()
-            else: break
+    def deal(self, deck):
+        while len(self.hand) < 6:
+            self.draw(deck)
 
 
     def discard(self):
-        cards = []
-        player.hand = ['H1','H2','H3','H4','H5']
-        deck.append(hand(len(hand -1)))
-        deck_count += 1
-        hand(len(hand -1)).pop
-        hand_count -= 1
+        deck.cards.append(self.hand[len(self.hand) - 1])
+        self.hand.pop()
 
-    def displayInfo(Player):
-        print self.hand, self.name
+    def displayInfo(self):
+        print self.name, self.hand
 
-John = Player("John", [])
-
-#
-# import random
-# 5cards =[]
-# while len(5cards) < 5:
-# cards = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-# rand_card = random.choice(cards)
-# suit = ['H','C','S','D']
-# rand_suit = random.choice(suit)
-# deck_card = rand_card + rand_suit
-# # it's too complicated to filter out all possible duplicates. you'd have to keep every generated comiinatino in memory
-# # then
-# print deck_card
-#     i = 0
-#     while i =< len(arr){
-#         if deck_card != arr[i]{
-#             i++;
-#         }
-#         else break;
-#     }
-#     5cards += deck_card
-# }
+KennyLoggins = Player('KennyLoggins', [])
+KennyLoggins.draw(Deck)
